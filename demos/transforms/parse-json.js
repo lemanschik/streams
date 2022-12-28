3a8980c8b69c0407226e1bc7518b71ab8549ab8c
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-28 09:14:49 +0100

    Update parse-json.js

--- demos/transforms/parse-json.js
+++ demos/transforms/parse-json.js
@@ -1,5 +1,5 @@
 // import { declareFunctionAsTransformStream } from 'direktspeed/nils'
 // declareFunctionAsTransformStream(JSON.parse);
-parseJSON() { return new TransformStream({ transform(chunk, controller) {
+export const parseJSON() => new TransformStream({ transform(chunk, controller) {
       controller.enqueue(JSON.parse(chunk));
-} }); };
+ }); };
