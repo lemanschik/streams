8cbe93565f8dac341e5f254e7145da61df9274fa
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-28 09:18:25 +0100

    Update split-stream.js
    Update: example

--- demos/transforms/split-stream.js
+++ demos/transforms/split-stream.js
@@ -1,15 +1,8 @@
-function splitStream(splitOn) {
-  let buffer = '';
-
-  return new TransformStream({
-    transform(chunk, controller) {
-      buffer += chunk;
+export const splitStream = (splitOn) => { let buffer = ''; return new TransformStream({
+    transform(chunk, controller) { buffer += chunk;
       const parts = buffer.split(splitOn);
       parts.slice(0, -1).forEach(part => controller.enqueue(part));
       buffer = parts[parts.length - 1];
     },
-    flush(controller) {
-      if (buffer) controller.enqueue(buffer);
-    }
-  });
-}
+    flush: (controller) => (buffer) && controller.enqueue(buffer),
+}); }
